feb19c0d87960ea0d09252d2e63734c293f9e268
sofysofy <standungan@gmail.com>
2022-08-03 14:13:26 +0800

    Commit 001 - first upload

---
+++ config.py
@@ -0,0 +1,19 @@
+
+train_dir = "";
+valid_dir = "";
+test_dir = "";
+
+img_transform = "";
+# { train, test}
+# resize, flip. crop, normalize
+
+
+device = "cpu";
+
+# train params
+model_arx = "";
+lossFunc = "";
+optim = "";
+epochs = 5;
+batch_size = 4;
+
