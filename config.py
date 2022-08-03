ba4d66b364997eb600cb6bfe762bb8c48d37cf4a
sofysofy <standungan@gmail.com>
2022-08-03 14:47:23 +0800

    add template files

--- config.py
+++ config.py
@@ -1,19 +1,22 @@
+train_dir = '..\..\Dataset\PlantDisease\Train'
+valid_dir = '..\..\Dataset\PlantDisease\Validation'
+test_dir = '..\..\Dataset\PlantDisease\Test'
 
-train_dir = "";
-valid_dir = "";
-test_dir = "";
-
-img_transform = "";
+img_transform = ""
 # { train, test}
 # resize, flip. crop, normalize
 
-
-device = "cpu";
+device = "cpu"
 
 # train params
-model_arx = "";
-lossFunc = "";
-optim = "";
-epochs = 5;
-batch_size = 4;
+model_arx = ""
+lossFunc = ""
+optim = ""
+epochs = 5
+batch_size = 4
 
+# validation
+save_threshold = 0.85
+
+# save model 
+filename = "" #modelName_time_acc_loss.pth
---
+++ train.py
@@ -0,0 +1,24 @@
+
+# load configuration
+
+# load dataset
+
+# create trainloader
+
+# load model from models folder
+    # implement arx :
+        # lenet --- done
+        # alexnet
+        # vgg
+        # resnet
+
+# create tensorboard
+
+# create training loop
+
+    # training process
+
+    # validation process if valid dir isn't empty
+
+    # save model with validation > save_threshold
+
