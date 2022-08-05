bcac83fdecedb59e24de5659d41de24e60baabcd
sofysofy <standungan@gmail.com>
2022-08-05 20:25:47 +0800

    add accuracy

---
+++ utilities/metrics.py
@@ -0,0 +1,10 @@
+import torch
+
+def accuracy(target, prediction):
+    with torch.no_grad():
+        pred = torch.argmax(prediction, dim=1)
+        assert pred.shape[0] == len(target)
+        correct = 0
+        correct += torch.sum(pred == target).item()
+        acc = correct / len(target)
+    return acc
